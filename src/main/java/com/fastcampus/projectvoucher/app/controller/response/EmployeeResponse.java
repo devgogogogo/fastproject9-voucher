5bec8d139d49fd849d4a6e3d710e5be152529ff3
kwihyeon <imleo322@gmail.com>
2025-09-19 01:17:19 +0900

    feat : employee api 간단 구현  - 사원 생성 조회 구현

---
+++ src/main/java/com/fastcampus/projectvoucher/app/controller/response/EmployeeResponse.java
@@ -0,0 +1,10 @@
+package com.fastcampus.projectvoucher.app.controller.response;
+
+public record EmployeeResponse(
+        Long no,
+        String name,
+        String position,
+        String department
+) {
+
+}
