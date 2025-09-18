7a2bf5b3e3e7a3f0d4ef8bf61d9bb0b2cec936ab
kwihyeon <imleo322@gmail.com>
2025-09-18 17:44:19 +0900

    첫 커밋

---
+++ src/test/java/com/fastcampus/projectvoucher/ProjectVoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectvoucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectVoucherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
